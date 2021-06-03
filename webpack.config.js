53ab0b90c35ea14bbd9f5ecf40b0038dd3d73fb1
Vitor Pamplona <vitor@vitorpamplona.com>
2021-06-03 11:35:17 -0400

    updating dist naming convention

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./lib/main.js",
   devtool: "source-map",
   output: {
-    filename: 'bbs-jxt.sdk.min.js',
+    filename: 'bbs-jxt-sdk.min.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'BBS_JXT',
     libraryTarget: 'umd',
